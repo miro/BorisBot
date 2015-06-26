2c29c9ca7da3d71f0dacee02ecbbe007821a6520
Miro Nieminen <miro.nieminen@futurice.com>
2015-06-26 16:08:23 +0300

    Add webhook to Heroku

--- config.js
+++ config.js
@@ -18,7 +18,7 @@ cfg.tgApiKey = process.env.BORISBOT_TELEGRAM_APIKEY;
 cfg.tgApiUrl = 'https://api.telegram.org/bot' + cfg.tgApiKey;
 
 // URL where the Telegram webhook will send POST requests. Change accordingly!
-cfg.webhookUrl = 'https://457450e5.ngrok.com/api/webhook'; // https://borisbot.herokuapp.com/api/webhook
+cfg.webhookUrl = 'https://borisbot.herokuapp.com/api/webhook';
 
 
 module.exports = cfg;
