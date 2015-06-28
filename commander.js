dd456f64d2a3219a565cb8ff5cac62c7f68bb63b
Miro Nieminen <miro.nieminen@futurice.com>
2015-06-28 15:42:41 +0300

    Add "@" prefix to username only if it is a username

--- commander.js
+++ commander.js
@@ -47,7 +47,7 @@ commander.handleWebhookEvent = function runUserCommand(msg) {
                     }).length;
 
                     // everyone doesn't have username set - use first_name in that case
-                    var username = _.isUndefined(msg.from.username) ? msg.from.first_name : msg.from.username;
+                    var username = _.isUndefined(msg.from.username) ? msg.from.first_name : ('@' + msg.from.username);
 
                     var userPosition = commander.getUserCurrentPosition(drinksCollection, userId);
 
