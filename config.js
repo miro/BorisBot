37b5ba3b06eb747f66077735dd508620b7eeb305
Miro Nieminen <miro.nieminen@futurice.com>
2015-06-28 15:53:44 +0300

    Fetch webhook URL via env var

--- config.js
+++ config.js
@@ -17,8 +17,9 @@ cfg.serverPort = process.env.PORT || 3000;
 cfg.tgApiKey = process.env.BORISBOT_TELEGRAM_APIKEY;
 cfg.tgApiUrl = 'https://api.telegram.org/bot' + cfg.tgApiKey;
 
-// URL where the Telegram webhook will send POST requests. Change accordingly!
-cfg.webhookUrl = 'https://borisbot.herokuapp.com/api/webhook';
+// URL where the Telegram webhook will send POST requests
+cfg.webhookUrl = process.env.BORISBOT_WEBHOOK_URL;
+
 
 
 module.exports = cfg;
