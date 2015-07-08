33d5c0fa11a2de5763281012ef6e4eaf85a42221
Miro Nieminen <miro.nieminen@futurice.com>
2015-07-08 20:25:32 +0300

    Separate functions from commander to new drinkController and utils -modules

--- commander.js
+++ commander.js
@@ -1,6 +1,5 @@
 // ### Handles all the bot commands
 
-var fs          = require('fs');
 var Promise     = require('bluebird');
 var request     = require('request');
 var moment      = require('moment-timezone');
@@ -8,10 +7,11 @@ var _           = require('lodash');
 
 var cfg         = require('./config');
 var db          = require('./database');
-var graph       = require('./graph');
 var botApi      = require('./botApi');
+var utils       = require('./utils');
 
 var userController      = require('./controllers/userController');
+var drinkController     = require('./controllers/drinkController');
 
 // set default timezone to bot timezone
 moment.tz.setDefault(cfg.botTimezone);
@@ -56,53 +56,28 @@ commander.handleWebhookEvent = function runUserCommand(msg) {
         // parse command & possible parameters
         var userInput = msg.text.split(' ');
         var userCommand = userInput.shift();
-        var userCommandParams = userInput.join(' ').substring(0,140); // TODO this limit should be done only for /kippis commands
+        var userCommandParams = userInput.join(' ');
 
 
         switch (userCommand.toLowerCase()) {
             case '/kalja':
             case '/kippis':
-                commander.registerDrink(msg.message_id, chatGroupId, chatGroupTitle, userId, userName, userCommandParams)
-                .then(function(returnMessage) {
-                    botApi.sendMessage(msg.chat.id, returnMessage);
-                    resolve();
-                })
-                .error(function(e) {
-                    botApi.sendMessage(msg.chat.id, 'Kippistely epäonnistui, yritä myöhemmin uudelleen');
-                    resolve();
-                });
+                drinkController.addDrink(msg.message_id, chatGroupId, chatGroupTitle, userId, userCallName, userCommandParams)
+                .then(resolve);
             break;
 
             case '/kaljoja':
-                // TODO "joista x viimeisen tunnin aikana"?
-
-                if (_eventIsFromGroup(msg)) {
-                    db.getTotalDrinksAmountForGroup(msg.chat.id)
-                    .then(function fetchOk(result) {
-                        var output = msg.chat.title + ' on tuhonnut yhteensä ' + result[0].count + ' juomaa!';
-                        botApi.sendMessage(msg.chat.id, output);
-                        resolve();
-                    });
-                }
-                else {
-                    db.getTotalDrinksAmount()
-                    .then(function fetchOk(result) {
-                        botApi.sendMessage(msg.chat.id, 'Kaikenkaikkiaan juotu ' + result[0].count + ' juomaa');
-                        resolve();
-                    });
-                }
+                drinkController.getDrinksAmount(userId, chatGroupId, chatGroupTitle, _eventIsFromGroup(msg))
+                .then(resolve);
             break;
 
             case '/otinko':
-                commander.getPersonalDrinkLog(userId)
-                .then(function(logString) {
-                    botApi.sendMessage(userId, logString);
-
+                drinkController.getPersonalDrinkLog(userId)
+                .then(function() {
                     if (_eventIsFromGroup(msg)) {
                         botApi.sendMessage(userId, 'PS: anna "' +
                             userCommand + '"-komento suoraan minulle, älä spämmää turhaan ryhmächättiä!');
                     }
-
                     resolve();
                 });
             break;
@@ -113,53 +88,19 @@ commander.handleWebhookEvent = function runUserCommand(msg) {
             // Takes one parameter, which changes the length of the graph
             case '/graafi':
             case '/histogrammi':
-
-                var dbFetchFunction = null;
-                var targetId = null;
-
-                if (_eventIsFromGroup(msg)) {
-                    dbFetchTimestampFunction = db.getFirstTimestampForGroup;
-                    dbFetchDrinksFunction = db.getGroupDrinkTimesSince;
-                    targetId = chatGroupId;
-                }
-                else {
-                    dbFetchTimestampFunction = db.getFirstTimestampForUser;
-                    dbFetchDrinksFunction = db.getPersonalDrinkTimesSince;
-                    targetId = userId;
-                }
-
-                dbFetchTimestampFunction(targetId)
-                .then(function setRange(result) {
-                    var startRangeMoment = moment(result[0]['min']);
-                    var dateRangeParameter = parseInt(userCommandParams.split(' ')[0], 10);
-
-                    if (!_.isNaN(dateRangeParameter)) {
-                        startRangeMoment = moment().subtract(dateRangeParameter,'days')
-                    }
-
-                    dbFetchDrinksFunction(targetId, startRangeMoment)
-                    .then(function createHistogramFromData(drinkTimestamps) {
-                        graph.makeHistogram(drinkTimestamps, startRangeMoment)
-                        .then(function histogramCreatedHandler(plotly) {
-                            var destinationFilePath = cfg.plotlyDirectory + 'latestGraph.png';
-                            _downloadFile(plotly.url + '.png', destinationFilePath, function () {
-                                botApi.sendPhoto(targetId, destinationFilePath);
-                                resolve();
-                            });
-                        });
-                    });
-                });
+                drinkController.drawGraph(userId, chatGroupId, _eventIsFromGroup(msg), userCommandParams)
+                .then(resolve);
             break;
 
             // Sends image of current state of Spänni's webcam
             // Triggering of this is only possible from the spännimobi group
             case '/webcam':
-            
+
                 if (_.isUndefined(cfg.webcamURL)) {
                     botApi.sendMessage(userId, 'Botille ei ole määritetty webcamin osoitetta!');
                     resolve();
                 } else {
-            
+
                     if (!_eventIsFromGroup(msg)) {
                         // this command can only be triggered from a group, since this command is
                         // limited to a certain users only, and for now we have no means of finding
@@ -186,7 +127,7 @@ commander.handleWebhookEvent = function runUserCommand(msg) {
                     }
 
                     // -> If we get here, we are good to go!
-                    _downloadFile(cfg.webcamURL, cfg.webcamDirectory + 'webcam.jpg', function() {
+                    utils.downloadFile(cfg.webcamURL, cfg.webcamDirectory + 'webcam.jpg', function() {
                         botApi.sendPhoto(chatGroupId, cfg.webcamDirectory + 'webcam.jpg');
                         resolve();
                     });
@@ -250,96 +191,15 @@ commander.handleWebhookEvent = function runUserCommand(msg) {
     });
 };
 
-commander.registerDrink = function(messageId, chatGroupId, chatGroupTitle, userId, userName, drinkType) {
-    // TODO create drinkController,move this to there
-
-    // fallback to 'kalja' if no drinkType is set
-    drinkType = !drinkType ? 'kalja' : drinkType;
-
-    return new Promise(function(resolve, reject) {
-        db.registerDrink(messageId, chatGroupId, userId, drinkType)
-        .then(function() {
-            db.getDrinksSinceTimestamp(_getTresholdMoment(), chatGroupId)
-            .then(function createReturnMessageFromCollection(drinksCollection) {
-
-                var drinksToday = drinksCollection.models.length;
-                var drinksTodayForThisUser = _.filter(drinksCollection.models, function(model) {
-                    return model.attributes.creatorId === userId;
-                }).length;
-
-                // # Form the message
-                var returnMessage = 'Kippis!!';
-
-                // was this todays first for the user?
-                if (drinksTodayForThisUser === 1) {
-                    returnMessage += ' Päivä käyntiin!';
-                }
-
-                // Is there a group title?
-                if (_.isNull(chatGroupTitle)) {
-                    returnMessage += ' Se olikin jo ' + drinksTodayForThisUser + '. tälle päivälle.\n';
-                }
-                else {
-                    returnMessage += ' Se olikin jo ryhmän ' + chatGroupTitle + ' ' + drinksToday +
-                    '. tälle päivälle, ja ' + drinksTodayForThisUser + '. käyttäjälle ' + userName + '.\n';
-                }
-
-                resolve(returnMessage);
-            });
-        });
-    });
-};
-
-
-commander.getPersonalDrinkLog = function(userId) {
-    // TODO sort by timestamp, better
-    return new Promise(function (resolve, reject) {
-
-        db.getDrinksSinceTimestampForUser(moment().subtract(2, 'day'), userId)
-        .then(function(collection) {
-            var message = 'Juomasi viimeisen 48h ajalta:\n\n';
-
-            _.each(collection.models, function(model) {
-                message += moment(model.get('timestamp')).format('HH:mm');
-                message += ' - ' + model.get('drinkType') + '\n';
-            });
-
-            message += '______________\n';
-            message += 'Yhteensä ' + collection.models.length + ' kpl';
-
-            resolve(message);
-        });
-    });
-};
 
 
 // Helper functions
 //
-var _getTresholdMoment = function() {
-    var treshold = moment().hour(9).minute(0);
-
-    var tresholdIsInFuture = treshold.isAfter(moment());
-    if (tresholdIsInFuture) {
-        treshold.subtract(1, 'day');
-    }
-
-    return treshold;
-};
 
 // Returns true, if this event is triggered from group
 var _eventIsFromGroup = function(msg) {
     return !_.isUndefined(msg.chat.title);
 };
 
-
-
-var _downloadFile = function(uri, filename, callback) {
-    request.head(uri, function(err, res, body) {
-        if (err) {
-            console.log('Error on file download!', err);
-        }
-        request(uri).pipe(fs.createWriteStream(filename)).on('close', callback);
-    });
-};
 
 module.exports = commander;
