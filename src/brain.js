417314cee94deeb5d7160cbfd7c72a2a7d2a60e8
Antti Kivimäki <majoriz11@gmail.com>
2016-05-21 23:40:47 +0300

    Improve /iltaa

--- src/brain.js
+++ src/brain.js
@@ -13,6 +13,7 @@ var Promise         = require('bluebird');
 var _               = require('lodash');
 
 var botApi          = require('./botApi');
+var explController  = require('./controllers/explController');
 
 
 var brain = {};
@@ -39,5 +40,21 @@ brain.announceDrinkLogsToGroups = function announceDrinkLogsToGroups(groupLogs) 
 
     return Promise.resolve();
 };
+
+brain.answerIltaa = function(event) {
+
+    if (Math.random()*100 < 10) {
+        explController.getRandomExpl(event);
+    } else {
+        var illat = ['iltaa','iltes','iltua','NÄYTÄ SITÄ ILTUA!','ILTAA',
+            'Hyvää iltaa.', 'NÄYTÄ HERRAN TÄHDEN SITÄ ILTUA!',
+            'Olisko tänään iltua?', '/iltaa <iltua>', '!expl iltua',
+            'Sinun kannattaa ensiksi täyttää profiilisi, että muut voivat tutustua sinuun.',
+            'velu: homo', '#spännimobi: kannattaa tulla kanavalle, niin saat tietoa tulevista tapahtumista',
+            'spinni: huono, mutta köyhä kerho', 'millan "panoilta"', 'AI KAUHEE'];
+
+        botApi.sendMessage({chat_id: event.targetId, text: _.sample(illat)});
+    }
+}
 
 module.exports = brain;
