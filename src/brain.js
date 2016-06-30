af5c1a44f5b3be09ec44e9035e4c299dda4cf554
Miro Nieminen <miro.nieminen@futurice.com>
2016-07-01 01:05:47 +0300

    Housecleaning - fix more linting errors

--- src/brain.js
+++ src/brain.js
@@ -1,14 +1,14 @@
 // # Brain
 //
 //      This module is reponsible for all the "natural" responses the bot has.
-//      There isn't much yet, but in future we would like to move all the "talk logic" from controllers into
-//      this module.
+//      There isn't much yet, but in future we would like to move all the "talk logic"
+//      from controllers into this module.
 //
 //      Controllers should only be responsible for fetching & modifying data in DB, and brain should
 //      output that data to the users.
 //
-//      Not to be mixed with talkbox.js; it is responsible for parsing "natural language" coming from users,
-//      this module is for creating "natural language" from the bot data.
+//      Not to be mixed with talkbox.js; it is responsible for parsing "natural language" coming
+//      from users, this module is for CREATING "natural language" from the bot data.
 var Promise         = require('bluebird');
 var _               = require('lodash');
 
@@ -19,7 +19,7 @@ var explController  = require('./controllers/explController');
 var brain = {};
 
 brain.announceDrinkLogsToGroups = function announceDrinkLogsToGroups(groupLogs) {
-    _.each(groupLogs, function(log, groupId) {
+    _.each(groupLogs, (log, groupId) => {
         var msgParts = [
             '*HUOMENTA, ja vielä kerran HUOO MEN TA! Uusi päivä!*',
             'Eilisen tykittelijät:',
@@ -42,19 +42,23 @@ brain.announceDrinkLogsToGroups = function announceDrinkLogsToGroups(groupLogs) 
 };
 
 brain.answerIltaa = function(event) {
-
-    if (Math.random()*100 < 10) {
+    if (Math.random() * 100 < 10) {
         explController.getRandomExpl(event);
     } else {
-        var illat = ['iltaa','iltes','iltua','NÄYTÄ SITÄ ILTUA!','ILTAA',
-            'Hyvää iltaa.', 'NÄYTÄ HERRAN TÄHDEN SITÄ ILTUA!',
+        var illat = [
+            'iltaa', 'iltes', 'iltua', 'NÄYTÄ SITÄ ILTUA!', 'ILTAA',
+            'Hyvää iltaa.', 'NÄYTÄ HERRAN TÄHDEN SITÄ ILTUA!', 'velu: rupale',
             'Olisko tänään iltua?', '/iltaa <iltua>', '!expl iltua',
             'Sinun kannattaa ensiksi täyttää profiilisi, että muut voivat tutustua sinuun.',
-            'velu: homo', '#spännimobi: kannattaa tulla kanavalle, niin saat tietoa tulevista tapahtumista',
-            'spinni: huono, mutta köyhä kerho', 'millan "panoilta"', 'AI KAUHEE'];
+            '#spännimobi: kannattaa tulla kanavalle, niin saat tietoa tulevista tapahtumista',
+            'spinni: huono, mutta köyhä kerho', 'millan "panoilta"', 'AI KAUHEE'
+        ];
 
-        botApi.sendMessage({chat_id: event.targetId, text: _.sample(illat)});
+        botApi.sendMessage({
+            chat_id: event.targetId,
+            text: _.sample(illat)
+        });
     }
-}
+};
 
 module.exports = brain;
