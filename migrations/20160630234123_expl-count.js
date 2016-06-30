49ae1ca2c9864e3a75a4f39192b7d15ffe2abbb7
Miro Nieminen <miro.nieminen@futurice.com>
2016-07-01 00:29:37 +0300

    [SCHEMA CHANGE]: Add lastEcho-timestamp to expl
    No idea if it is completely wrong to alter already committed migration file,
    but my defense is that it wasn't merged to dev branch yet

--- migrations/20160630234123_expl-count.js
+++ migrations/20160630234123_expl-count.js
@@ -4,11 +4,16 @@ exports.up = function(knex) {
         table.integer('echoCount')
             .defaultTo(0)
             .comment('How many times this expl has been requested');
+
+        table.timestamp('lastEcho')
+            .nullable()
+            .comment('Last time this expl was echoed somewhere');
     });
 };
 
 exports.down = function(knex) {
     return knex.schema.table('expls', table => {
         table.dropColumn('echoCount');
+        table.dropColumn('lastEcho');
     });
 };
